3ac09d6e8c69a265293bd826c529d3203c10afef
waston <sunzh95@hotmail.com>
2018-03-07 17:36:35 +0800

    fix stack and queue

--- daily/data_struct_algorithm/stack_and_queue.py
+++ daily/data_struct_algorithm/stack_and_queue.py
@@ -7,11 +7,38 @@
 @time: 11:38
 @note:  
 
-栈是一种运算受限制的链表, 也有共享栈等, FILO
+栈是一种运算受限制的线性表, 也有共享栈等, FILO, 递归的本质也是栈
+push, pop, 都是 T(n)=O(1), 此处是基于数组的实现方式
 
-队列也是运算受限的线性表, 也有双端队列, FIFO
+队列也是运算受限的线性表, 也有双端队列, FIFO, 常用于消息队列
 双端队列比队列每个节点多储存了 _prev, 代表了前一个节点
+push, pop, 都是 T(n)=O(1), 此处是基于链表的实现方式
+
 """
+from typing import Iterable
+
+
+class Stack(object):
+    def __init__(self, init=None):
+        if isinstance(init, Iterable):
+            self.items = [v for v in init]
+        elif init:
+            self.items = [init]
+        else:
+            self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    @property
+    def top(self):
+        return self.items[-1]
+
+    def pop(self):
+        return self.items.pop()
+
+    def __repr__(self):
+        return str(self.items)
 
 
 class Node(object):
@@ -20,10 +47,10 @@ class Node(object):
         self._next = pnext
 
 
-class Stack(object):
-    def __init__(self, head=None):
-        if head:
-            self.head = head
+class Queue(object):
+    def __init__(self, node=None):
+        if node:
+            self.head = node
             self.length = 1
         else:
             self.head = None
@@ -40,28 +67,6 @@ class Stack(object):
         self.length += 1
         return True
 
-    def pop(self):
-        if not self.head:
-            return None
-        else:
-            current_node = self.head
-            for _ in range(self.length - 2):
-                current_node = current_node._next
-            value = current_node._next.value
-            current_node._next = None
-            self.length -= 1
-            return value
-
-    def __repr__(self):
-        values = []
-        current_node = self.head
-        while current_node:
-            values.append(current_node.value)
-            current_node = current_node._next
-        return str(values)
-
-
-class Queue(Stack):
     def popleft(self):
         if not self.head:
             return None
@@ -70,18 +75,25 @@ class Queue(Stack):
             self.head = current_node._next
             value = current_node.value
             del current_node
+            self.length -= 1
             return value
 
-    def pop(self):
-        raise ValueError('Queue not allow this method, use "popleft"')
+    def __repr__(self):
+        current_node = self.head
+        values = []
+        while current_node:
+            values.append(current_node.value)
+            current_node = current_node._next
+        return str(values)
 
 
 if __name__ == '__main__':
     print('Stack:')
     stack = Stack()
-    stack.push(Node(1))
-    stack.push(Node(2))
+    stack.push(1)
+    stack.push(2)
     print(stack)
+    print(stack.pop())
     print(stack.pop())
     print(stack)
 
